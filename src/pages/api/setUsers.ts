47e173c4ac771877c43da42a958ff9dba8ca6abf
Gabriel Dutra <gfhdutra@gmail.com>
2021-08-17 14:27:12 -0300

    add database clients

--- src/pages/api/setUsers.ts
+++ src/pages/api/setUsers.ts
@@ -1,10 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Client } from '@notionhq/client'
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY })
 
+const notion = new Client({ auth: process.env.NOTION_API_KEY })
 export default async function setUsers(req: NextApiRequest, res: NextApiResponse) {
   const { user, email, password } = req.body
+
+  async function createClientDatabase(pageId) {
+    await notion.databases.create({
+      parent: {
+        page_id: pageId
+      },
+      title: [
+        {
+          type: "text",
+          text: {
+            content: "Clientes",
+            link: null
+          }
+        }
+      ],
+      properties: {
+        "Id": {
+          "title": {}
+        },
+        "Nome Completo": {
+          "rich_text": {}
+        },
+        "Cpf": {
+          "rich_text": {}
+        },
+        "E-mail": {
+          "rich_text": {}
+        },
+        "Telefone": {
+          "rich_text": {}
+        },
+        "Endereço": {
+          "rich_text": {}
+        },
+      }
+    })
+      .then(() => {
+        res.status(201).json({ message: "criado com sucesso" })
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({ message: 'Ops, algo deu errado!' })
+      })
+  }
 
   await notion.pages.create({
     parent: {
@@ -43,7 +87,8 @@ export default async function setUsers(req: NextApiRequest, res: NextApiResponse
       }
     }
   })
-    .then(() => {
+    .then(response => {
+      createClientDatabase(response.id)
       res.status(201).json({ message: 'Sucesso!' })
     })
     .catch(error => {
